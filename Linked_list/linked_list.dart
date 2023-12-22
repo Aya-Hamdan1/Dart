5741fb784c807c8e5c2b7debe8dff52f66a38380
Aya-Hamdan1 <s11821764@stu.najah.edu>
2023-12-23 01:24:07 +0200

    Update linked_list.dart

--- Linked_list/linked_list.dart
+++ Linked_list/linked_list.dart
@@ -1,5 +1,23 @@
-class Node<T> {
-  Node({required this.value, this.next});
-  T value;
-  Node<T>? next;
+import 'Node.dart';
+
+class LinkedList<E> {
+  Node<E>? head;
+  Node<E>? tail;
+
+  bool get isEmpty => head == null;
+
+  void push(E value) {
+  head = Node(value: value, next: head);
+  tail ??= head;
+}
+
+ void append(E value) {
+  if (isEmpty) {
+    push(value);
+    return;
+  }
+  tail!.next = Node(value: value);
+  tail = tail!.next;
+}
+
 }
